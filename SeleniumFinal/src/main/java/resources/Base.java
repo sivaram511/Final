32ad0a479b0781406bb42a6e20bf37faf4557265
sivaram511 <swamy@LAPTOP-49ALIVKJ>
2021-10-05 11:15:37 +0530

    ram

--- SeleniumFinal/src/main/java/resources/Base.java
+++ SeleniumFinal/src/main/java/resources/Base.java
@@ -30,7 +30,7 @@ public class Base {
 		System.out.println(p.getProperty("url"));
 		driver.get(p.getProperty("url"));
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		
 		return driver;
 		
