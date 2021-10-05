781facfb8e3d61a4cd682630d1f54247378e01bf
sivaram511 <swamy@LAPTOP-49ALIVKJ>
2021-10-05 11:12:59 +0530

    ram

--- SeleniumFinal/src/main/java/resources/Base.java
+++ SeleniumFinal/src/main/java/resources/Base.java
@@ -23,13 +23,8 @@ public class Base {
 		//driver=new ChromeDriver();
 		WebDriverManager.chromedriver().setup();
 
-		driver=new ChromeDriver();// initializing the browser
-		System.out.println("hai22ram");
-
-		
-		
-
-		
+		driver=new ChromeDriver();
+			
 		br=new BufferedReader(new FileReader(proppath));
 		p.load(br);
 		System.out.println(p.getProperty("url"));
